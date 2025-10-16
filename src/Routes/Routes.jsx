d2c0704e4fd70f024cd7866bd1d8240c65c55060
mjh-shikder <mjhsikder@gmail.com>
2025-10-16 17:33:44 +0600

    error page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root";
+import ErrorPage from "../pages/Error/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World   dfsdf</div>,
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
   },
 ]);
