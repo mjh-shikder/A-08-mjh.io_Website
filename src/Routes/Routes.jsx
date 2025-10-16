3f90c7043b1838085ea52b5ac2669e13d695c56f
mjh-shikder <mjhsikder@gmail.com>
2025-10-16 18:39:16 +0600

    Navbar and functional navlinks added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,29 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../pages/Root";
 import ErrorPage from "../pages/Error/ErrorPage";
+import Home from "../pages/Home";
+import Apps from "../pages/Apps";
+import Installation from "../pages/Installation";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/installation",
+        Component: Installation,
+      },
+    ],
   },
 ]);
---
+++ src/pages/Installation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Installation = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center mt-24'>Installation Page</h1>
+        </div>
+    );
+};
+
+export default Installation;
