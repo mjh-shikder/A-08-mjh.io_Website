411f3bef2b8aa1593c8770b71cf24ce0fd979428
mjh-shikder <mjhsikder@gmail.com>
2025-10-16 20:53:09 +0600

    Footer added

--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Apps = () => {
     return (
-        <div>
+        <div >
             <h1 className='text-4xl text-center mt-24'>Apps page</h1>
         </div>
     );
