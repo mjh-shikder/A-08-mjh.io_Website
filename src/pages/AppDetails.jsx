8210d43cf7f05f063ba3d85a3132edb0b9a8f218
mjh-shikder <mjhsikder@gmail.com>
2025-10-17 23:33:21 +0600

    addign Rechart on apps details

--- src/pages/AppDetails.jsx
+++ src/pages/AppDetails.jsx
@@ -1,68 +1,126 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router';
-import downloadIcon from './../assets/icon-downloads.png'
-import avgRatingIcon from './../assets/icon-ratings.png'
-import totalReviewIcon from './../assets/icon-review.png'
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
+import downloadIcon from "./../assets/icon-downloads.png";
+import avgRatingIcon from "./../assets/icon-ratings.png";
+import totalReviewIcon from "./../assets/icon-review.png";
+import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 const AppDetails = () => {
+  
+  const { id } = useParams();
+  const appId = parseInt(id);
+  const allData = useLoaderData();
+  const singleData = allData.find((app) => app.id === appId);
+  const {
+    image,
+    title,
+    companyName,
+    description,
+    size,
+    reviews,
+    ratingAvg,
+    downloads,
+    ratings,
+  } = singleData;
 
-    const { id } = useParams();
-    const appId = parseInt(id)
-    const allData = useLoaderData()
-    const singleData = allData.find(app => app.id === appId);
-    const {image, title, companyName, description, size, reviews, ratingAvg, downloads, ratings  } = singleData
-
-    console.log(id, singleData);
+    // console.log(ratings.map(rating => console.log(rating.count)));
     
-    const firstDownloadDigit = downloads.toString()[0];
-    const firstTwoReviewDigits = reviews.toString().slice(0,-2);
+//     const ratingDataArry = ratings.map(rating => rating)
+//     const ratingDataCount = ratingDataArry.map(rat=>rat.count)
+//     const ragingDataName = ratingDataArry.map(rat=>rat.name)
 
-    return (
-        <div className='bg-[#f5f5f5] '>
-            <div className='flex items-center gap-10 py-20 w-10/12 mx-auto'>
-                <div className='w-3/12 bg-white rounded-xl shadow-md ' >
-                    <img className='' src={image} alt="app-logo" />
-                </div>
-                <div>
-                    <h1 className='text-3xl font-semibold text-gray-800 my-2.5'>{title}</h1>
-                    <p className='text-gray-500'>Developed by <span className='text-indigo-600 font-semibold'>{companyName} </span> </p>
+//      console.log(ratingDataCount, ragingDataName);
 
-                    
+//     const Data = ragingDataName.map((name, index) => ({
+//         name: name,
+//         count: ratingDataCount[index]
+//     }))
 
-                    <div className='border-t border-gray-300 mt-5 w-[900px] '></div>
+// console.log(Data);
 
 
-                    <div className='flex gap-10 my-5'>
 
-                        <div className='flex flex-col items-center '>
-                            <img className='w-[30px] mb-1.5' src={downloadIcon} alt="Download-icon" />
-                            <p className='text-xs text-gray-600'>Downloads</p>
-                            <h1 className='text-3xl font-bold text-gray-800'>{firstDownloadDigit}M</h1>
-                        </div>
-                        <div className='flex flex-col items-center '>
-                            <img className='w-[30px] mb-1.5' src={avgRatingIcon} alt="ratings-icon" />
-                            <p className='text-xs text-gray-600'>Average Ratings</p>
-                            <h1 className='text-3xl font-bold text-gray-800'>{ratingAvg}</h1>
-                        </div>
-                        <div className='flex flex-col items-center '>
-                            <img className='w-[30px] mb-1.5' src={totalReviewIcon} alt="reviw-icon" />
-                            <p className='text-xs text-gray-600'>Total Reviews</p>
-                            <h1 className='text-3xl font-bold text-gray-800'>{firstTwoReviewDigits}K</h1>
-                        </div>
+ const data = ratings
+    
 
-                    </div>
-                    <button className='btn bg-[#00cf8d] text-white px-6 '>Install Now ({size} MB)</button>
-                </div>
-            </div>
+  const firstDownloadDigit = downloads.toString()[0];
+  const firstTwoReviewDigits = reviews.toString().slice(0, -2);
 
-            <div className='border-t border-gray-300 py-5  '></div>
-
-            <div>
-
-            </div>
-            
+  return (
+    <div className="bg-[#f5f5f5] ">
+      <div className="flex items-center gap-10 py-20 w-10/12 mx-auto">
+        <div className="w-3/12 bg-white rounded-xl shadow-md ">
+          <img className="" src={image} alt="app-logo" />
         </div>
-    );
+        <div>
+          <h1 className="text-3xl font-semibold text-gray-800 my-2.5">
+            {title}
+          </h1>
+          <p className="text-gray-500">
+            Developed by{" "}
+            <span className="text-indigo-600 font-semibold">
+              {companyName}{" "}
+            </span>{" "}
+          </p>
+
+          <div className="border-t border-gray-300 mt-5 w-[900px] "></div>
+
+          <div className="flex gap-10 my-5">
+            <div className="flex flex-col items-center ">
+              <img
+                className="w-[30px] mb-1.5"
+                src={downloadIcon}
+                alt="Download-icon"
+              />
+              <p className="text-xs text-gray-600">Downloads</p>
+              <h1 className="text-3xl font-bold text-gray-800">
+                {firstDownloadDigit}M
+              </h1>
+            </div>
+            <div className="flex flex-col items-center ">
+              <img
+                className="w-[30px] mb-1.5"
+                src={avgRatingIcon}
+                alt="ratings-icon"
+              />
+              <p className="text-xs text-gray-600">Average Ratings</p>
+              <h1 className="text-3xl font-bold text-gray-800">{ratingAvg}</h1>
+            </div>
+            <div className="flex flex-col items-center ">
+              <img
+                className="w-[30px] mb-1.5"
+                src={totalReviewIcon}
+                alt="reviw-icon"
+              />
+              <p className="text-xs text-gray-600">Total Reviews</p>
+              <h1 className="text-3xl font-bold text-gray-800">
+                {firstTwoReviewDigits}K
+              </h1>
+            </div>
+          </div>
+          <button className="btn bg-[#00cf8d] text-white px-6 ">
+            Install Now ({size} MB)
+          </button>
+        </div>
+      </div>
+
+      <div className="border-t border-gray-300 py-5  "></div>
+          <h2 className="font-semibold text-gray-700 mb-4">Ratings</h2>
+          
+          <div className=" relative h-[600px] ">
+              
+        <div className="rotate-90 absolute">
+          {/* <ResponsiveContainer width="100%" height="100%"> */}
+                  <BarChart width={500} height={500} data={data}>
+                      <XAxis dataKey={'name'}></XAxis>
+                      {/* <YAxis ></YAxis> */}
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+          {/* </ResponsiveContainer> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppDetails;
