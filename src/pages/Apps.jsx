f99cd6c12dfa9a3d6ce56beae52e28b796491a13
mjh-shikder <mjhsikder@gmail.com>
2025-10-19 16:01:37 +0600

    adding filter option

--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -8,17 +8,37 @@ const Apps = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredApps, setFilteredApps] = useState(allData);
 
+  const [sortOption, setSortOptions] = useState("High-Low")
+
   useEffect(() => {
-    const filtered = allData.filter((app) => app.title.toLowerCase().includes(searchQuery.toLocaleLowerCase()));
+    let filtered = allData.filter((app) => app.title.toLowerCase().includes(searchQuery.toLocaleLowerCase()));
+
+
+    filtered = [...filtered].sort( (a, b) => {
+      if (sortOption === "High-Low") {
+        return b.downloads - a.downloads;
+      }
+      else {
+        return a.downloads - b.downloads;
+      }
+    })
+
+
     setFilteredApps(filtered)
 
-  }, [searchQuery, allData]);
+  }, [searchQuery, sortOption, allData]);
 
   // Handle search 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
   
+
+  // sort option
+  const handleSortChange = e => {
+    setSortOptions(e.target.value);
+
+  }
 
   return (
     <div className="bg-[#f5f5f5]  ">
@@ -30,7 +50,17 @@ const Apps = () => {
       </p>
 
       <div className="flex justify-between w-10/12 mx-auto my-7 items-center ">
+        
         <h1 className="text-xl font-semibold text-gray-700 ">({filteredApps.length})Apps Found</h1>
+
+         <select
+            value={sortOption}
+            onChange={handleSortChange}
+            className="border border-gray-300 rounded-md px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="High-Low">Downloads: High to Low</option>
+            <option value="Low-High">Downloads: Low to High</option>
+          </select>
 
         <label className="input">
           <svg
