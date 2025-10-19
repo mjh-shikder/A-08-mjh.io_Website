a3209e5eb6b7d77f950d6e7c76429a156cd81dd0
mjh-shikder <mjhsikder@gmail.com>
2025-10-19 15:39:54 +0600

    app not found error page updated

--- src/pages/Error/NoAppsFound.jsx
+++ src/pages/Error/NoAppsFound.jsx
@@ -7,8 +7,11 @@ const NoAppsFound = () => {
     <div className="flex flex-col items-center justify-center md:px-0 px-4 ">
       <img className="m-10 md:px-0 px-4 " src={noAppsFound} alt="" />
       <h1 className="text-3xl font-semibold text-gray-700 mt-7">
-        Oops, Page Not Found!
-      </h1>
+        Oops!! APP NOT FOUND
+          </h1>
+          <p className="text-gray-500 my-3.5">
+        The App you are searching is not found on our system.
+          </p>
       <Link to={"/"}>
         <button className="btn px-7 mt-7 bg-gradient-to-br text-white from-[#6630e4] to-[#bb8bff]">
           Go Back
